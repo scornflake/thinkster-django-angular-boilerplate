085de5dcf34cd9dcec7d692f8ff15531972ffb77
Neil Clayton <neil@shinywhitebox.com>
2014-12-24 17:51:34 +1300

    - Up to creating Account via HTML, doesn't Submit properly (I get a 405 back)

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,14 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
     '',
-
+    url(r'^api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 )
